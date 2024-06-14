bb3047bb945f0f9cb40dc534c39b96b3e78297b9
ThamaliNirmala <tnirmalaadasooriya@gmail.com>
2024-06-14 19:01:35 +0530

    Tailwind css and ant design configured

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import ViewProduts from './components/ViewProduts';
+import AddProduct from './components/AddProduct';
+import EditProduct from './components/EditProduct';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path='/' element={< ViewProduts/>}/>
+        <Route path='/add' element={< AddProduct/>}/>
+        <Route path='/edit' element={< EditProduct/>}/>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ frontend/src/components/ViewProduts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ViewProduts = () => {
+  return (
+    <div>ViewProduts</div>
+  )
+}
+
+export default ViewProduts
